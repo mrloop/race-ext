31bc5ad1fc3ddd199b5e55c2a9eec959db5b1f56
mrloop <ewan@mrloop.com>
2017-09-18 19:12:37 +0100

    Get event id

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,15 @@ const initDesktop = function() {
     if(index == 0) {
       addHeader(row);
     } else {
-      let containerElement = document.createElement('div');
-      row.appendChild(document.createElement('td').appendChild(containerElement));
-      initApp(containerElement);
+      let td = document.createElement('td');
+      row.appendChild(td);
+      let link = row.querySelector('[data-event-id]');
+      if(link) {
+        let id = link.getAttribute('data-event-id');
+        let containerElement = document.createElement('div');
+        td.appendChild(containerElement);
+        initApp(containerElement);
+      }
     }
   });
 };
@@ -18,10 +24,14 @@ const initDesktop = function() {
 const initMobile = function() {
   Array.from(table.getElementsByClassName('events--mobile__row')).forEach((row, index) => {
     if(index > 0) {
-      let buttons = row.getElementsByClassName('events--mobile__link__buttons')[0];
-      let containerElement = document.createElement('div');
-      buttons.appendChild(document.createElement('div').appendChild(containerElement));
-      initApp(containerElement);
+      let link = row.querySelector('[data-event-id]')
+      if(link) {
+        let id = link.getAttribute('data-event-id');
+        let buttons = row.getElementsByClassName('events--mobile__link__buttons')[0];
+        let containerElement = document.createElement('div');
+        buttons.appendChild(document.createElement('div').appendChild(containerElement));
+        initApp(containerElement);
+      }
     }
   });
 };
