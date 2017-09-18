898cf8eb88c26ec348475dd8f9caf59c8a125f2b
mrloop <ewan@mrloop.com>
2017-09-18 14:36:44 +0100

    Initiate app instances for each event.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,57 @@
 import App from './main';
 import { ComponentManager, setPropertyDidChange } from '@glimmer/component';
 
-const app = new App();
-const containerElement = document.getElementById('app');
+const table = document.getElementsByClassName('article--events__table')[0];
 
-setPropertyDidChange(() => {
-  app.scheduleRerender();
-});
+const initDesktop = function() {
+  Array.from(table.getElementsByClassName('events--desktop__row')).forEach((row, index) => {
+    if(index == 0) {
+      addHeader(row);
+    } else {
+      let containerElement = document.createElement('div');
+      row.appendChild(document.createElement('td').appendChild(containerElement));
+      initApp(containerElement);
+    }
+  });
+};
 
-app.registerInitializer({
-  initialize(registry) {
-    registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager);
-  }
-});
+const initMobile = function() {
+  Array.from(table.getElementsByClassName('events--mobile__row')).forEach((row, index) => {
+    if(index > 0) {
+      let buttons = row.getElementsByClassName('events--mobile__link__buttons')[0];
+      let containerElement = document.createElement('div');
+      buttons.appendChild(document.createElement('div').appendChild(containerElement));
+      initApp(containerElement);
+    }
+  });
+};
 
-app.renderComponent('race-ext', containerElement, null);
+const addHeader = function(row) {
+  const heading = document.createElement('th');
+  const text = document.createTextNode('race lib');
+  heading.appendChild(text);
+  row.appendChild(heading);
+}
 
-app.boot();
+const initApp = function(containerElement) {
+  let app = new App();
+
+  setPropertyDidChange(() => {
+    app.scheduleRerender();
+  });
+
+  app.registerInitializer({
+    initialize(registry) {
+      registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager);
+    }
+  });
+
+  app.renderComponent('race-ext', containerElement, null);
+
+  app.boot();
+}
+
+initDesktop();
+initMobile();
+
+
